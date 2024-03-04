616537ba716526923e30c6a1e1ffce127f07637f
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2024-03-04 23:06:10 +0800

    set eslint and prettier configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,5 @@
+// ESLint 配置 - 代码规范检查
+module.exports = {
+    // 关闭所有不必要或可能与 Prettier 冲突的规则，仅启用检测潜在错误的规则
+    extends: ['plugin:prettier/recommended']
+};
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nodejs-express-demo
+
 一个基于 Node.js Express 框架构建的 RESTful API 入门 Demo 范例。
